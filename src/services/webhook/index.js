a72fd6c44da6699da1d64bfec35a963e27810e7c
Alan Verbner <alverbner@gmail.com>
2016-05-28 13:41:00 -0300

    Fix imports

--- src/services/webhook/index.js
+++ src/services/webhook/index.js
@@ -2,9 +2,9 @@
 
 const hooks = require('./hooks')
 const _ = require('lodash')
-const messageParser = require('../../lib/messages/message-parser')
-const stateMachine = require('../../lib/messages/response-state-machine')
-const superagent = require('../../lib/util/superagent-promisified')
+const messageParser = require('../../lib/messages/message-parser.js')
+const stateMachine = require('../../lib/messages/response-state-machine.js')
+const superagent = require('../../lib/util/superagent-promisified.js')
 const co = require('co')
 
 function sendResponseToFacebook(to, message) {
